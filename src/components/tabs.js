d54033721f384e5eb4e91fdbf6d71a71b78769f5
napu <nahuel@wonderup.io>
2025-05-14 20:09:13 -0300

    tabs

---
+++ src/components/tabs.js
@@ -0,0 +1,57 @@
+import { sumHeightsOfElements } from '../utils'
+
+const tabsComponent = document.querySelector('[data-component="tabs"]')
+
+if (!!tabsComponent) {
+  console.log('COMPONENT: TABS')
+
+  const tabsButtons = tabsComponent.querySelectorAll('[data-tabs="button"]')
+
+  //initial state
+  collapseAccordion(tabsButtons)
+  expandAccordion(tabsButtons[0])
+
+  //add event listeners
+  tabsButtons.forEach((tab) => {
+    tab.addEventListener('click', (e) => {  
+      const target = e.currentTarget
+      const isActive = target.classList.contains('is-active')
+      if (!isActive) {
+        collapseAccordion(tabsButtons)
+        expandAccordion(target)
+      }
+    })
+  })
+}
+
+function expandAccordion(tab) {
+  tab.classList.add('is-active')
+  const accordion = tab.querySelector('[data-tabs="accordion"]')
+  accordion.style.height = sumHeightsOfElements(accordion.childNodes) + 'px'
+
+  const parent = tab.parentElement
+  const tabs = parent.childNodes
+  tabs.forEach((tab) => {
+    tab.style.borderBottomRightRadius = '0rem'
+    tab.style.borderTopRightRadius = '0rem'
+  })
+  const previousSibling = tab.previousElementSibling
+  const nextSibling = tab.nextElementSibling
+  if (previousSibling) {
+    console.log('previousSibling', previousSibling)
+    previousSibling.style.borderBottomRightRadius = '1rem'
+  }
+  if (nextSibling) {
+    console.log('nextSibling', nextSibling)
+    nextSibling.style.borderTopRightRadius = '1rem'
+  }
+}
+
+function collapseAccordion(tabs) {
+  tabs.forEach((tab) => {
+    tab.classList.remove('is-active')
+    const accordion = tab.querySelector('[data-tabs="accordion"]')
+    accordion.style.height = '0rem'
+  })
+}
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './components/lenis-scroll.js'
 import './components/navbar.js'
 import './components/sticky-text.js'
+import './components/tabs.js'
 import './components/reviews.js'
 import './components/stats.js'
 import './components/faqs.js'
